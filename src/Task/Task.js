98a7ca666a5081b81be49388c1c72989eebd4aec
Art <art9024@gmail.com>
2025-02-01 11:33:49 +0300

    styles

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -3,12 +3,14 @@ import './Task.css'
 
 export default function Task(props) {
   return (
-    <div className="task">
-      <p className="task__title">Задача: <strong>{props.title}</strong></p>
-      <p className="task__text">Описание: {props.text} </p>
-      <p className="task__mail">E-mail: {props.mail} </p>
-      <p className="task__user">Пользователь: {props.user} </p>
-      <p className="task__done">Выполнено: {props.isDone ? 'да' : 'нет'} </p>
+    <div className='task-container'>
+      <div className="task">
+        <p className="task__title">Задача: <strong>{props.title}</strong></p>
+        <p className="task__text">Описание: {props.text} </p>
+        <p className="task__mail">E-mail: {props.mail} </p>
+        <p className="task__user">Пользователь: {props.user} </p>
+        <p className="task__done">Выполнено: {props.isDone ? 'да' : 'нет'} </p>
+      </div>
     </div>
   );
 }
